0429775fcfc8f9028fb4f579e018d31f909faeff
Hamidreza Chehrepak <hamidreza1977chehrepak@gmail.com>
2026-09-21 16:16:49 +0330

    Add C event generator

---
+++ c_engine/src/event_generator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("SIMULATED_SECURITY_EVENT\n");
+    printf("event_type=PROCESS_EXECUTION\n");
+    printf("status=SIMULATED\n");
+    printf("severity=medium\n");
+
+    return 0;
+}
